e9a61a1abf38c27e2c0647e5fc083aad5773d9b6
Clement-Praxinos <clement.berthaud@praxinos.coop>
2019-06-08 15:20:23 +0200

    feat: implemented optimized constexpr C++14 char op, better complexity support

--- coal/Source/coal/Include/coal/coal.char.h
+++ coal/Source/coal/Include/coal/coal.char.h
@@ -11,94 +11,100 @@
 #pragma once
 
 
-namespace ULIS {
-namespace _CT {
-
-
+namespace coal {
+namespace cpp11 {
 /////////////////////////////////////////////////////
-// Compile-Time Constexpr cpp0x compliant Char Operations
+// c++11 compliant Constexpr Functions
+typedef  const char* cstr;
+constexpr bool  is_digit    ( char c )                                          { return  c >= '0' && c <= '9'; }
+constexpr int   stoi_impl   ( const char* s, int v = 0 )                        { return  *s ? is_digit( *s ) ? stoi_impl( s+1, (*s-'0') + v*10 ) : -1 : v; }
+constexpr int   stoi        ( const char* s )                                   { return  stoi_impl( s ); }
+constexpr int   strlen      ( const char* s )                                   { return  *s ? 1 + strlen(s + 1) : 0; }
+constexpr bool  streq       ( char const * a, char const * b )                  { return *a == *b && ( *a == '\0' || streq( a + 1, b + 1 ) ); }
+constexpr bool  is_number   ( const char* s )                                   { return  *s ? is_digit( *s ) ? is_number( s+1 ) : false : true; }
+constexpr int   indexof     ( const char* w, const char* s, int f=0, int q=0 )  { return s[f+q] ? w[q] ? s[f+q] == w[q] ? q == strlen( w ) - 1 ? f : indexof( w, s, f, q+1 ) : indexof( w, s, f, q+1 ) : indexof( w, s, f+1 ) : -1; }
+} // namespace cpp11
 
 
-/* check if char is a digit at compile time */
-constexpr bool
-is_digit( char c )
-{
-    return c >= '0' && c <= '9';
-}
-
-
-/* convert cstring to int at compile time, implementation, fail if not a number */
+namespace cpp14 {
+/////////////////////////////////////////////////////
+// c++14 compliant Constexpr Functions
 constexpr int
-stoi_impl( const char* s, int value = 0 )
+strlen( const char* s )
 {
-    return *s ? is_digit( *s ) ? stoi_impl( s+1, (*s-'0') + value*10 ) : -1 : value;
+    for( int i = 0;; ++i ) if( !s[i] ) return  i;
 }
 
 
-/* convert cstring to int at compile time */
-constexpr int
-stoi( const char* s )
-{
-    return stoi_impl( s );
-}
-
-
-/* compute cstring length at compile time */
-constexpr int
-strlen( const char* str )
-{
-    return *str ? 1 + strlen(str + 1) : 0;
-}
-
-
-/* compare two cstrings at compile time */
 constexpr bool
 streq( char const * a, char const * b )
 {
-    return *a == *b && ( *a == '\0' || streq( a + 1, b + 1 ) );
+    int lena = strlen( a );
+    int lenb = strlen( b );
+    if( lena != lenb ) return  false;
+
+    for( int i = 0; i < lena; ++i )
+        if( a[i] != b[i] )
+            return  false;
+
+    return  true;
 }
 
 
-/* check if cstring is a number at compile time */
+constexpr bool
+is_digit( char c )
+{
+    return  c >= '0' && c <= '9';
+}
+
+
 constexpr bool
 is_number( const char* s )
 {
-    return *s ? is_digit( *s ) ? is_number( s+1 ) : false : true;
+    for( int i = 0; i < strlen( s ); ++i )
+        if( !is_digit( s[i] ) )
+            return  false;
+
+    return  true;
 }
 
 
-/* find index of word occurence from start in cstring at compile time */
+constexpr int
+stoi( const char* s )
+{
+    int r = 0;
+    int l = strlen( s );
+    for( int i = 0; i < strlen( s ); ++i )
+        r = r * 10 + int( s[i] - '0' );
+    return  r;
+}
+
+
 constexpr int
 indexof( const char* word, const char* str, int from = 0 )
 {
-    int ret = -1;
-    bool exit = false;
-    for( int i = from; i <= strlen( str ); ++i ) {
-        if( exit ) break;
-        for( int j = 0; j <= strlen( word ); ++j ) {
+    int ls = strlen( str );
+    int lw = strlen( word );
+
+    for( int i = from; i < ls; ++i )
+    {
+        for( int j = 0; j < lw; ++j )
+        {
             if( str[i+j] != word[j] )
                 break;
 
-            if( j == strlen( word ) - 1 )
+            if( j == lw - 1 )
             {
-                ret = i;
-                exit = true;
-                break;
+                return  i;
             }
         }
     }
-    return ret;
-}
 
-/* short version ( breaks recursion depth limit )*/
-constexpr int indof( cstr w, cstr s, int f=0, int q=0 )
-{
-    return s[f+q] ? w[q] ? s[f+q] == w[q] ? q == strlen( w ) - 1 ? f : indof( w, s, f, q+1 ) : indof( w, s, f, q+1 ) : indof( w, s, f+1 ) : -1;
+    return  -1;
 }
+} // namespace cpp14
+} // namespace coal
+
 
 //constexpr const int ct_char_index_without_digits ( char c, const char* s ) { int cind = 0; int lpos = 0; while(1) { if( s[cind] == c ) { break; } if( !is_digit( s[cind] ) ) { ++lpos; } ++cind; } return lpos; }
-
-
-} // namespace _CT
-} // namespace ULIS
 
