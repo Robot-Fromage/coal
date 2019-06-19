8c1fd3a567a60f3d7512a2d4e9988bb8a9553b79
Clement-Praxinos <clement.berthaud@praxinos.coop>
2019-06-19 12:38:25 +0200

    infeat: implemented coal string list as tuple

--- coal/Source/coal_test/Source/coal_test.cpp
+++ coal/Source/coal_test/Source/coal_test.cpp
@@ -9,8 +9,9 @@
 */
 
 #include <iostream>
-#include <assert.h>
 #include <coal_core>
+#include <tuple>
+#include <assert.h>
 
 struct FInfo
 {
@@ -22,9 +23,12 @@ struct FInfo
 };
 
 
+coal str = coalMakeFromString( "654,5141,21132,478,45,2888" );
+
+
 template< typename X, typename T, T... Nums >
 constexpr  auto
-GenStartSeq_Imp( X, std::integer_sequence< T, Nums... > )
+GenStartSeq_Imp( std::integer_sequence< T, Nums... > )
 {
     return  std::integer_sequence< int, X::value().querySplitWordStart( ",", Nums ) ... >();
 }
@@ -33,24 +37,67 @@ template< typename X >
 constexpr  auto
 GenStartSeq( X )
 {
-    using forwardConstexprArg0 = coalConstexprWrapper( X::value() );
-    return  GenStartSeq_Imp( forwardConstexprArg0{}, std::make_integer_sequence< int, X::value().querySplitNum( "," ) >() );
+    return  GenStartSeq_Imp< X >( std::make_integer_sequence< int, X::value().querySplitNum( "," ) >() );
 }
 
 
-coal str = coalMakeFromString( "654,5141,21132,478,45,2888" );
+template< typename X, typename Y, typename T, T... Nums >
+constexpr  auto
+SplitIntoTuple_Imp( std::integer_sequence< T, Nums... > )
+{
+    //return  std::make_tuple( X::value().substring< X::value().querySplitWordSize( Y::value(), Nums ) >( X::value().querySplitWordStart( Y::value(), Nums ) ) ... );
+    return  std::make_tuple( coalSplitElem( X::value(), Y::value(), Nums ) ... );
+}
+
+
+template< typename X, typename Y >
+constexpr  auto
+SplitIntoTuple( X, Y )
+{
+    return  SplitIntoTuple_Imp< X, Y >( std::make_integer_sequence< int, X::value().querySplitNum( Y::value() ) >() );
+}
+
+
+constexpr int coal_test_str()
+{
+    return 0;
+}
+
+
+constexpr int coal_test_int()
+{
+    return 0;
+}
+
+
+constexpr int coal_test_list()
+{
+    return 0;
+}
 
 
 int main()
 {
-    constexpr int num1 = coalSplitElem( str, ",", 0 ).toInt();
-    constexpr int num2 = coalSplitElem( str, ",", 1 ).toInt();
-    constexpr int num3 = coalSplitElem( str, ",", 2 ).toInt();
+    coal num1 = coalSplitElem( str, ",", 0 );
+    coal num2 = coalSplitElem( str, ",", 1 );
+    coal num3 = coalSplitElem( str, ",", 2 );
     //...
     //constexpr auto jeeee = std::integer_sequence< int, str.querySplitWordStart( ",", 0 ), str.querySplitWordStart( ",", 1 ) >();
     using forwardConstexprArg0 = coalConstexprWrapper( str );
     coal jajo1 = GenStartSeq( forwardConstexprArg0{} );
     coal test = coalMakeFromInt( 569 );
+
+    coal list = std::make_tuple( coalSplitElem( str, ",", 0 ), coalSplitElem( str, ",", 1 ), coalSplitElem( str, ",", 2 ) );
+    coal e0 = std::get< 0 >( list );
+    static_assert( e0 == num1, " .... " );
+    constexpr char ok1 = num1[0];
+    constexpr char ok2 = e0[0];
+    static_assert( '6' == ok2, "ok" );
+
+    coal sep = coalMakeFromString( "," );
+    using forwardConstexprArg1 = coalConstexprWrapper( "," );
+    constexpr auto list2 = SplitIntoTuple( forwardConstexprArg0{}, forwardConstexprArg1{} );
+    static_assert( std::get< 0 >( list2 ) == num1, "nop" );
 
     return 0;
 }
