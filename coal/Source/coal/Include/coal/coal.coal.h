33213159d5f7885f070ebe881ae723b805398756
Robot-Fromage <code@clementberthaud.com>
2019-07-24 17:52:27 +0200

    infix: ignored qualifiers

--- coal/Source/coal/Include/coal/coal.coal.h
+++ coal/Source/coal/Include/coal/coal.coal.h
@@ -73,20 +73,20 @@ struct  coal_t
 
     // Advanced Template
     /* substring */
-    static constexpr const char coal_impl_getchar_substring( const char* str, int start, int num ) { return str[num+start]; }
+    static constexpr char coal_impl_getchar_substring( const char* str, int start, int num ) { return str[num+start]; }
     template< int L, typename T, T... Nums > static constexpr const coal_t< L > make_coal_substring_impl(  const char* str, int start, std::integer_sequence< T, Nums... > ) { return { coal_impl_getchar_substring( str, start, Nums ) ..., '\0' }; }
     template< int L > static constexpr const coal_t< L > make_coal_substring( const char* str, int start = 0 ) { return make_coal_substring_impl< L >( str, start, std::make_integer_sequence< int, L-1 >() ); }
     template< int L > constexpr const coal_t< L + 1 > substring( int start = 0 ) const { return make_coal_substring< L + 1 >( s, start );}
 
     /* append */
-    static constexpr const char coal_impl_getchar_append( const char* strA, const char* strB, int lenA, int num ) { return num < lenA ? strA[num] : strB[num-lenA]; }
+    static constexpr char coal_impl_getchar_append( const char* strA, const char* strB, int lenA, int num ) { return num < lenA ? strA[num] : strB[num-lenA]; }
     template< int L, typename T, T... Nums > static constexpr const coal_t< L > make_coal_append_impl(  const char* strA, const char* strB, std::integer_sequence< T, Nums... > ) { return { coal_impl_getchar_append( strA, strB, strlen( strA ), Nums ) ..., '\0' }; }
     template< int L > static constexpr const coal_t< L > make_coal_append( const char* strA, const char* strB ) { return make_coal_append_impl< L >( strA, strB, std::make_integer_sequence< int, L-1 >() ); }
     template< int L > constexpr const coal_t< N + L - 1 > append( const char (&str)[L] ) const { return make_coal_append< N + L - 1 >( s, str ); }
     template< int L > constexpr const coal_t< N + L - 1 > append( const coal_t< L >& i ) const { return  append( i.s ); }
 
     /* prepend */
-    static constexpr const char coal_impl_getchar_prepend( const char* strA, const char* strB, int lenB, int num ) { return num < lenB ? strB[num] : strA[num-lenB]; }
+    static constexpr char coal_impl_getchar_prepend( const char* strA, const char* strB, int lenB, int num ) { return num < lenB ? strB[num] : strA[num-lenB]; }
     template< int L, typename T, T... Nums > static constexpr const coal_t< L > make_coal_prepend_impl(  const char* strA, const char* strB, std::integer_sequence< T, Nums... > ) { return { coal_impl_getchar_prepend( strA, strB, strlen( strB ), Nums ) ..., '\0' }; }
     template< int L > static constexpr const coal_t< L > make_coal_prepend( const char* strA, const char* strB ) { return make_coal_prepend_impl< L >( strA, strB, std::make_integer_sequence< int, L-1 >() ); }
     template< int L > constexpr const coal_t< N + L - 1 > prepend( const char (&str)[L] ) const { return make_coal_prepend< N + L - 1 >( s, str ); }
@@ -108,7 +108,7 @@ template< int N > constexpr coal_t< N > make_coal_from_cstr( const char* str ) {
 
 /////////////////////////////////////////////////////
 // coal maker from int
-constexpr const char
+constexpr char
 make_coal_from_int_impl_getchar( int i, int num )
 {
     int result = strip_most_significant_digits( i, num );
